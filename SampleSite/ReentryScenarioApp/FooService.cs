c56632f6a5bbd491d2d624a0c4fe5d11f17f68ce
jsakamoto <junichi.jp@gmail.com>
2020-09-26 20:36:05 +0900

    Add sample app.

---
+++ SampleSite/ReentryScenarioApp/FooService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Toolbelt.Blazor;
+
+namespace ReentryScenarioApp
+{
+    public class FooService : IDisposable
+    {
+        private readonly HttpClient HttpClient;
+
+        private readonly HttpClientInterceptor HttpClientInterceptor;
+
+        public FooService(
+            IHttpClientFactory httpClientFactory,
+            HttpClientInterceptor httpClientInterceptor
+        )
+        {
+            this.HttpClient = httpClientFactory.CreateClient("for FooService");
+            this.HttpClientInterceptor = httpClientInterceptor;
+
+            this.HttpClientInterceptor.BeforeSend += HttpClientInterceptor_BeforeSend;
+            this.HttpClientInterceptor.BeforeSendAsync += HttpClientInterceptor_BeforeSendAsync;
+            this.HttpClientInterceptor.AfterSend += HttpClientInterceptor_AfterSend;
+            this.HttpClientInterceptor.AfterSendAsync += HttpClientInterceptor_AfterSendAsync;
+        }
+
+        private void HttpClientInterceptor_BeforeSend(object sender, HttpClientInterceptorEventArgs e)
+        {
+            Console.WriteLine("HttpClientInterceptor.BeforeSend");
+        }
+
+        private async Task HttpClientInterceptor_BeforeSendAsync(object sender, HttpClientInterceptorEventArgs e)
+        {
+            Console.WriteLine("HttpClientInterceptor.BeforeSendAsync-1");
+
+            var sampleTextB = await this.HttpClient.GetStringAsync("content/sample-text-B.txt");
+            Console.WriteLine($"Sample Text B: {sampleTextB}");
+
+            Console.WriteLine("HttpClientInterceptor.BeforeSendAsync-2");
+        }
+
+        private void HttpClientInterceptor_AfterSend(object sender, HttpClientInterceptorEventArgs e)
+        {
+            Console.WriteLine("HttpClientInterceptor.AfterSend");
+        }
+
+        private async Task HttpClientInterceptor_AfterSendAsync(object sender, HttpClientInterceptorEventArgs e)
+        {
+            Console.WriteLine("HttpClientInterceptor.AfterSendAsync-1");
+            await Task.Delay(3000);
+            Console.WriteLine("HttpClientInterceptor.AfterSendAsync-2");
+        }
+
+        public void Dispose()
+        {
+            this.HttpClientInterceptor.BeforeSend -= HttpClientInterceptor_BeforeSend;
+            this.HttpClientInterceptor.BeforeSendAsync -= HttpClientInterceptor_BeforeSendAsync;
+            this.HttpClientInterceptor.AfterSend -= HttpClientInterceptor_AfterSend;
+            this.HttpClientInterceptor.AfterSendAsync -= HttpClientInterceptor_AfterSendAsync;
+            this.HttpClient.Dispose();
+        }
+    }
+}
---
+++ SampleSite/ReentryScenarioApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace ReentryScenarioApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddHttpClientInterceptor();
+            builder.Services.AddScoped<FooService>();
+            builder.Services.AddHttpClient("for FooService", client =>
+            {
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            });
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            }.EnableIntercept(sp));
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
