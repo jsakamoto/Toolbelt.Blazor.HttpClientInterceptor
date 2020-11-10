1c16f3acf76616d5adb9cef27072c73549e0651c
jsakamoto <junichi.jp@gmail.com>
2020-11-10 18:29:39 +0900

    Add unit test to reproduce the problem that events are invoked twice.

---
+++ Toolbelt.Blazor.HttpClientInterceptor.Test/Internals/NullHttpMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Toolbelt.Blazor.Test.Internals
+{
+    internal class NullHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.NoContent);
+            return Task.FromResult(responseMessage);
+        }
+    }
+}
